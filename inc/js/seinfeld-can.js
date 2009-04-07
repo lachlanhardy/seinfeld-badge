2cc2f6587884e7d98672a4fdceab6725fd74589b
Lachlan Hardy <lachlan@lachstock.com.au>
2009-04-08 00:04:52 +1000

    Adding basic table action

--- inc/js/seinfeld-can.js
+++ inc/js/seinfeld-can.js
@@ -2,7 +2,6 @@ function seinfeldCan() {
     var username = "lachlanhardy";
     $.getJSON("http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22http%3A%2F%2Fwww.calendaraboutnothing.com%2F~" + username + "%22%20and%20xpath%3D'%2F%2Fdiv%5B%40id%3D%22stats%22%5D%2Fdiv'%0A%20%20%20%20&format=json&callback=?", function(data){
       var currentStreak = $("<p>My current streak of successive days committing public code is </p>").append(data.query.results.div[0].span[1].content);
-      // var longestHref = data.query.results.div[1].a.href;
       
       var longestAnchor = $("<a/>").attr("href", data.query.results.div[1].a.href).text(data.query.results.div[1].a.span[1].content);
       
@@ -11,4 +10,14 @@ function seinfeldCan() {
       $("#seinfeld-can").append(currentStreak).append(longestStreak);
       
   }); 
+  
+  $.getJSON("http://pipes.yahoo.com/pipes/pipe.run?_id=bDVGIHwj3hGmWk5qZMag4A&_render=json&_callback=?", function(calendar){
+      console.log(calendar.value.items[0].content);
+      
+      var table = $("<div id=\"table\"/>").html(calendar.value.items[0].content);
+      $("#seinfeld-can").append(table);
+  });
+  
+  
+  
 }
