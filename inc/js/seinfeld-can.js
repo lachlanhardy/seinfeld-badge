5099e5458bd45cadc9f9b57f2cc21641c3b463d0
Lachlan Hardy <lachlan@lachstock.com.au>
2009-04-18 13:07:48 +1000

    It lives.

--- inc/js/seinfeld-can.js
+++ inc/js/seinfeld-can.js
@@ -1,15 +1,15 @@
 function seinfeldCan(username) {
     var username = "lachlanhardy";
-    $.getJSON("http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22http%3A%2F%2Fwww.calendaraboutnothing.com%2F~" + username + "%22%20and%20xpath%3D'%2F%2Fdiv%5B%40id%3D%22stats%22%5D%2Fdiv'%0A%20%20%20%20&format=json&callback=?", function(data){
-      var currentStreak = $("<p>My current streak of successive days committing public code is </p>").append(data.query.results.div[0].span[1].content);
-      
-      var longestAnchor = $("<a/>").attr("href", data.query.results.div[1].a.href).text(data.query.results.div[1].a.span[1].content);
-      
-      var longestStreak = $("<p>and my longest streak is </p>").append(longestAnchor);
-
-      $("#seinfeld-can").append(currentStreak).append(longestStreak);
-      
-  }); 
+    // $.getJSON("http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22http%3A%2F%2Fwww.calendaraboutnothing.com%2F~" + username + "%22%20and%20xpath%3D'%2F%2Fdiv%5B%40id%3D%22stats%22%5D%2Fdiv'%0A%20%20%20%20&format=json&callback=?", function(data){
+    //       var currentStreak = $("<p>My current streak of successive days committing public code is </p>").append(data.query.results.div[0].span[1].content);
+    //       
+    //       var longestAnchor = $("<a/>").attr("href", data.query.results.div[1].a.href).text(data.query.results.div[1].a.span[1].content);
+    //       
+    //       var longestStreak = $("<p>and my longest streak is </p>").append(longestAnchor);
+    // 
+    //       $("#seinfeld-can").append(currentStreak).append(longestStreak);
+    //       
+    //   }); 
   
  $.getJSON("http://pipes.yahoo.com/pipes/pipe.run?_id=bDVGIHwj3hGmWk5qZMag4A&_render=json&_callback=?", function(calendar){
       
@@ -19,6 +19,16 @@ function seinfeldCan(username) {
       $("th.monthName", table).attr("colspan", "8");
       
       $("#seinfeld-can").append(table);
+      
+      $("td.progressed", table).each(function(){
+            var that = $(this);
+            console.log(that.width());
+            var x = $("<div class=\"xmarksthespot\"/>").css("height", that.height()).css("width", that.width());
+            x.append("<img src=\"http://calendaraboutnothing.com/images/x_1.png\" height=\"80%\" width=\"50%\">")
+            that.append(x);
+        });
   });
+  
+    
   
 }
